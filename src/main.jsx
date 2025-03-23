27bdab22a127e0b273188bdc579d3300ab1425a1
lukasus55 <lukas.kostyk@gmail.com>
2025-03-23 21:49:48 +0100

    Fixed pathing issues

--- src/main.jsx
+++ src/main.jsx
@@ -7,7 +7,7 @@ import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <HelmetProvider>
-    <BrowserRouter>
+    <BrowserRouter basename="/formula-stats">
       <App />
     </BrowserRouter>
   </HelmetProvider>
