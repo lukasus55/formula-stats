27b0e4c7e262ab48524d813c29a04921ee8fd54c
lukasus55 <lukas.kostyk@gmail.com>
2025-03-27 14:12:34 +0100

    Moved normalizeCSS to separate file

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,7 @@ import { HashRouter } from "react-router-dom";
 import { HelmetProvider } from "react-helmet-async";
 import App from './App.jsx';
 import './index.css';
+import './normalize.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <HelmetProvider>
