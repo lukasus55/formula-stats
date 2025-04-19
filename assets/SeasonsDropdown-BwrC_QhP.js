96b5a376465a98901b489f1462fa4b518525993a
lukasus55 <lukas.kostyk@gmail.com>
2025-04-19 17:56:43 +0200

    Updates

--- assets/SeasonsDropdown-BwrC_QhP.js
+++ assets/SeasonsDropdown-BwrC_QhP.js
@@ -1,4 +1,4 @@
-import{d as i,i as p,e as x,u as m,r as u,j as e}from"./index-CJv-lc15.js";/**
+import{d as i,i as p,e as x,u as m,r as u,j as e}from"./index-ukMYrEms.js";/**
  * @license lucide-react v0.479.0 - ISC
  *
  * This source code is licensed under the ISC license.
--- assets/index-79_pYNkC.js
+++ assets/index-79_pYNkC.js
@@ -1,4 +1,4 @@
-import{k as ne,r as D,R as Tt}from"./index-CJv-lc15.js";var ut={exports:{}},lt={};/**
+import{k as ne,r as D,R as Tt}from"./index-ukMYrEms.js";var ut={exports:{}},lt={};/**
  * @license React
  * use-sync-external-store-shim.production.js
  *
